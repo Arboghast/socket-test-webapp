d00a963c0ce041c1eb301c29f1d175dc18d4dbd2
Arboghast <BlueBubbleBeam@outlook.com>
2020-12-06 19:17:28 -0500

    upload test webapp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import socket from './socket.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.num = 0;
+    console.log("hit");
+  }
+
+  componentDidMount = () =>{
+    socket.on('createLobbyResponse', ({lobbyCode})=>{
+      console.log(lobbyCode);
+    });
+
+    socket.on('lobbyUpdate', ({users, error})=>{
+      console.log(users,error);
+        for(let i = 0; i < users.length; i++)
+        {
+          console.log(users[i]);
+        }
+    });
+
+    socket.on('raceInit', ({prompt, error})=>{
+      console.log(prompt,error);
+    });
+
+    socket.on('updateText', ({playerName, percentage, placement, error})=>{
+      console.log(playerName, percentage, placement, error);
+    });
+
+  };
+
+
+  render() {return (
+      <div className="App">
+        <button onClick={()=>{socket.emit('createLobby', { username: "mela"});}}>CREATE</button>
+
+        <button onClick={()=>{socket.emit('joinLobby', {lobbyCode: "arceux", username: "monkeies"});}}>JOIN</button>
+
+        <button onClick={()=>{socket.emit('leaveLobby', {lobbyCode: "arceux"});}}>LEAVE</button>
+
+        <button onClick={()=>{socket.emit('toggleReady', {lobbyCode: "arceux"});}}>TOGGLE</button>
+
+        <button onClick={()=>{socket.emit('startGame', {lobbyCode: "arceux"});}}>START</button>
+
+        <button onClick={()=>{socket.emit('letterTyped', {lobbyCode: "arceux", percentage: ++this.num });}}>START</button>
+
+        <button onClick={()=>{socket.emit('returnLobby', {lobbyCode: "arceux"});}}>RETURN</button>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/socket.js
@@ -0,0 +1,4 @@
+import io from 'socket.io-client';
+//'http://34.69.41.196:8000'
+let socket = io.connect('http://35.226.86.221:8000');
+export default socket;

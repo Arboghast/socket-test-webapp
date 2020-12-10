8ce96a1dd3583f4b77f4d2d64c8debb791832b3f
Arboghast <BlueBubbleBeam@outlook.com>
2020-12-09 22:53:29 -0500

    Goodbye World!

--- src/App.js
+++ src/App.js
@@ -2,51 +2,55 @@ import React from 'react';
 import socket from './socket.js';
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.num = 0;
     console.log("hit");
   }
 
-  componentDidMount = () =>{
-    socket.on('createLobbyResponse', ({lobbyCode})=>{
+  componentDidMount = () => {
+    socket.on('createLobbyResponse', ({ lobbyCode }) => {
       console.log(lobbyCode);
     });
 
-    socket.on('lobbyUpdate', ({users, error})=>{
-      console.log(users,error);
-        for(let i = 0; i < users.length; i++)
-        {
-          console.log(users[i]);
-        }
+    socket.on('lobbyUpdate', ({ users, error }) => {
+      console.log(users, error);
+      for (let i = 0; i < users.length; i++) {
+        console.log(users[i]);
+      }
     });
 
-    socket.on('raceInit', ({prompt, error})=>{
-      console.log(prompt,error);
+    socket.on('raceInit', ({ prompt, error }) => {
+      console.log(prompt, error);
     });
 
-    socket.on('updateText', ({playerName, percentage, placement, error})=>{
+    socket.on('updateText', ({ playerName, percentage, placement, error }) => {
       console.log(playerName, percentage, placement, error);
     });
 
   };
 
 
-  render() {return (
+  render() {
+    return (
       <div className="App">
-        <button onClick={()=>{socket.emit('createLobby', { username: "mela"});}}>CREATE</button>
+        <button onClick={() => { socket.emit('newUser', { username: "lima" }); }}>INIT</button>
 
-        <button onClick={()=>{socket.emit('joinLobby', {lobbyCode: "arceux", username: "monkeies"});}}>JOIN</button>
+        <button onClick={() => { socket.emit('newUser', { username: "monkeies" }); }}>INIT</button>
 
-        <button onClick={()=>{socket.emit('leaveLobby', {lobbyCode: "arceux"});}}>LEAVE</button>
+        <button onClick={() => { socket.emit('createLobby', {}); }}>CREATE</button>
 
-        <button onClick={()=>{socket.emit('toggleReady', {lobbyCode: "arceux"});}}>TOGGLE</button>
+        <button onClick={() => { socket.emit('joinLobby', { lobbyCode: "arceux" }); }}>JOIN</button>
 
-        <button onClick={()=>{socket.emit('startGame', {lobbyCode: "arceux"});}}>START</button>
+        <button onClick={() => { socket.emit('leaveLobby', { lobbyCode: "arceux" }); }}>LEAVE</button>
 
-        <button onClick={()=>{socket.emit('letterTyped', {lobbyCode: "arceux", percentage: ++this.num });}}>START</button>
+        <button onClick={() => { socket.emit('toggleReady', { lobbyCode: "arceux" }); }}>TOGGLE</button>
 
-        <button onClick={()=>{socket.emit('returnLobby', {lobbyCode: "arceux"});}}>RETURN</button>
+        <button onClick={() => { socket.emit('startGame', { lobbyCode: "arceux" }); }}>START</button>
+
+        <button onClick={() => { socket.emit('letterTyped', { lobbyCode: "arceux", percentage: ++this.num }); }}>INC</button>
+
+        <button onClick={() => { socket.emit('kickPlayer', { lobbyCode: "arceux", playerName: "monkeies" }); }}>KICK</button>
       </div>
     );
   }
--- src/socket.js
+++ src/socket.js
@@ -1,4 +1,3 @@
 import io from 'socket.io-client';
-//'http://34.69.41.196:8000'
-let socket = io.connect('http://35.226.86.221:8000');
+let socket = io.connect('http://localhost:8000');
 export default socket;
